589d209bb9b02eee7ab4ae94355db1bfafa3a90b
electron226 <electron226@gmail.com>
2014-11-05 21:31:53 +0900

    for git pages.

--- package.sh
+++
@@ -1,1 +0,0 @@
-zip -r package.zip _locales css icon src *.json *.zip *.html *.txt
--- src/content_scripts/getScrollPosition.js
+++
@@ -1,5 +0,0 @@
-/** ページのスクロール量を取得するのに使用する */
-var obj = new Object();
-obj.x = document.documentElement.scrollLeft || document.body.scrollLeft;
-obj.y = document.documentElement.scrollTop || document.body.scrollTop;
-obj; // executeScriptのコールバック関数に返る値
